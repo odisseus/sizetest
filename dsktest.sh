51adccb765e78d101cf4e8def519b050a856d8d9
odisseus <odisseus@users.noreply.github.com>
2015-06-25 04:31:36 +0300

    Improved progress messages. Added intermediary checks.

--- dsktest.sh
+++ dsktest.sh
@@ -1,43 +1,61 @@
 #!/bin/bash
 #Requires Bash 4 or newer
-#Requires units command: http://linux.die.net/man/1/units
 
 #$1 --- target directory
 #$2 --- count of blocks
-#$3 --- block size
+#$3 --- block size, format as recognized by dd command
+
+function log_to_stderr(){
+	>&2 echo $@
+}
+
+function check_checksums(){
+	local PASSED=1
+	for FILENAME in "${!CHECK_SUMS[@]}"
+	do 
+		EXPECTED_CHECKSUM=${CHECK_SUMS[$FILENAME]}
+		ACTUAL_CHECKSUM=$(md5sum "$FILENAME" | awk '{print $1}')
+		if [ $EXPECTED_CHECKSUM == $ACTUAL_CHECKSUM ]
+		then
+			log_to_stderr "$FILENAME -- OK"
+		else
+			log_to_stderr "$FILENAME -- checksum mismatch! Actual checksum: $ACTUAL_CHECKSUM"
+			PASSED=0
+		fi
+	done
+	echo $PASSED
+}
+
+function exit_if_checksum_mismatch(){
+	if [ $(check_checksums) == 1 ]
+	then
+		log_to_stderr "Check passed!"
+	else
+		log_to_stderr "Check failed!"
+		exit 3
+	fi
+}
 
 declare -A CHECK_SUMS
 
-echo "Creating $2 blocks of size $3..."
+log_to_stderr "Creating $2 blocks of size $3..."
+CHECK_STOP=1
 for i in $(seq 1 $2)
 do
-	echo $i
+	log_to_stderr $i
 	BLOCKFILE=$(mktemp --tmpdir="$1" block.$i.XXXX)
 	CHECKSUM=$(dd if=/dev/urandom bs="$3" count=1 | tee $BLOCKFILE | md5sum | awk '{print $1}')
-	echo "Block $i created in $BLOCKFILE with checksum $CHECKSUM"
+	log_to_stderr "Block $i created in $BLOCKFILE with checksum $CHECKSUM"
 	CHECK_SUMS[$BLOCKFILE]=$CHECKSUM
-done
-
-PASSED=1
-echo "Checking checksums..."
-for FILENAME in "${!CHECK_SUMS[@]}"
-do 
-	EXPECTED_CHECKSUM=${CHECK_SUMS[$FILENAME]}
-	ACTUAL_CHECKSUM=$(md5sum "$FILENAME" | awk '{print $1}')
-	if [ $EXPECTED_CHECKSUM == $ACTUAL_CHECKSUM ]
+	if [ $i -eq $CHECK_STOP ]
 	then
-		echo "$BASENAME -- OK"
-	else
-		echo "$BASENAME -- checksum mismatch! Actual checksum: $ACTUAL_CHECKSUM"
-		PASSED=0
+		log_to_stderr "Checking checksums for the first $i blocks so far..."
+		exit_if_checksum_mismatch
+		CHECK_STOP=$(( $CHECK_STOP * 2 ))
 	fi
 done
 
-if [ $PASSED -eq 1 ]
-then
-	echo "Check passed!"
-else
-	echo "Check failed!"
-	exit 3
-fi
+log_to_stderr "Checking checksums..."
+
+exit_if_checksum_mismatch
 
